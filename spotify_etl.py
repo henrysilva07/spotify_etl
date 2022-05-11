ce29cafeb5d3bb9bb95ac3a1ae0c7af9c6a6ae72
henrysilva07 <henry131307@gmail.com>
2022-05-11 17:14:49 -0300

    Criando a estrutura para album e artist

--- spotify_etl.py
+++ spotify_etl.py
@@ -10,7 +10,27 @@ from datetime import timedelta
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = "Henry" # your Spotify username
-TOKEN = 'BQCFyiNg8IgjHN4ThdBrzSXJOLFynipvaj-NrQKK0alNGWULC8cnsGXbHG5gSghpMkWXYRlwcLOXZSVDHUQCv4X-zL-LsTBlgkduig6BG-oApQuqbbYPIFCgIFBiMO4SaR-YqanB-hhbBAFNmBwWfhvFCjOEgmG_FEi4i3Ev'
+TOKEN = 'BQCHgDT-RCggex3sSM-3tYBlelssAhkIbWkxaiGQXJtR7CIWwTLXYRjXDAIypH30wr4Z1iVOCWXMOqGZNtnMLaYLl9JtY7FzpFi-5BkK8s_2k0VUuM7sd-Uv3F8gT0TyRSyfYNT8sXW1r6f0UEWXHBIATDljX7B_7rhqLv6t'
+
+
+def check_valid_data(df: pd.DataFrame) -> bool:
+    if df.empty:
+        print("No songs downloaded. Finishing process")
+
+        return False
+
+    #Primary key check
+    if pd.series(df[0]).is_unique:
+        pass
+    else:
+        raise Exception("Primary key check is violated")
+
+    if df.isnull().values.any():
+        raise Exception("Null value found")
+    else:
+        pass
+    pass
+
 
 if __name__ == "__main__":
     # Extract part of the ETL process
@@ -30,48 +50,10 @@ if __name__ == "__main__":
         headers=headers)
 
 
+
     data = r.json()
 
-    song_names = []
-    artist_names = []
-    albums_music = []
-    played_at_list = []
-    year_releases =[]
-    timestamps = []
-
-    for song in data['items']:
-
-       song_names.append(song["track"]["name"])
-       artist_names.append(song["track"]["album"]["artists"][0]["name"])
-       albums_music.append(song["track"]["album"]['name'])
-       year_releases.append(song["track"]["album"]['release_date'])
-       played_at_list.append(song["played_at"])
-       timestamps.append(song["played_at"][0:10])
-
-    song_dict = {
-        "song_name": song_names,
-        "artist_name": artist_names,
-        "album_music": albums_music,
-        "year_release" : year_releases,
-        "played_at": played_at_list,
-        "timestamp": timestamps
-    }
-
-    # Creating the Album Data Structure:
-    album_list = []
-    for song in data['items']:
-        album_id = song['track']['album']['id']
-        album_name = song['track']['album']['name']
-        album_release_date = song['track']['album']['release_date']
-        album_total_track = song['track']['album']['total_tracks']
-        album_url = song['track']['external_urls']['spotify']
-        album_attributes = {'album_id': album_id, 'name': album_name, 'release_date': album_release_date,
-                         'total_tracks': album_total_track, 'url': album_url}
-        album_list.append(album_information)
-
-
     # Creating Song Data Structure
-
     song_list = []
     for song in data['items']:
         song_id = song['track']['id']
@@ -87,11 +69,44 @@ if __name__ == "__main__":
         song_list.append(song_attributes)
 
 
-   # df_song = pd.DataFrame( data = song_dict , columns = ['song_name', 'artist_name', 'album_music', 'year_release',
-                                                            'played_at', 'timestamp'])
+    #Create Artist Data Struture
+    artist_list = []
+    for song in data['items']:
+        artist_id = song['track']['artists'][0]['id']
+        artist_name = song['track']['artists'][0]['name']
+        artist_url = song['track']['external_urls']['spotify']
+        song_time_played = song['played_at']
+        artist_attributes = {'artist_id': artist_id , 'artist_name': artist_name, 'artist_url': artist_url,
+                             'song_time_played': song_time_played}
+        artist_list.append(artist_attributes)
 
-    #df_song.to_csv("songs.csv", index = False)
+    album_list = []
+    for song in data['items']:
+        album_id = song['track']['album']['id']
+        album_name = song['track']['album']['name']
+        album_release_date = song['track']['album']['release_date']
+        album_total_tracks = song['track']['album']['total_tracks']
+        song_time_played = song['played_at']
+        album_url = song['track']['album']['external_urls']['spotify']
+        album_attributes = {'album_id': album_id, 'name': album_name, 'release_date': album_release_date,
+                         'total_tracks': album_total_tracks, 'url': album_url, 'song_time_played': song_time_played}
+        album_list.append(album_attributes)
 
+
+
+    # We will need to do some cleaning and add our Unique ID for the Track
+    # Then load into SQLite3 from the dataframe
+
+    album_df = pd.DataFrame.from_dict(album_list)
+    album_df = album_df.drop_duplicates(  subset=['album_id'] )
+
+    artist_df = pd.DataFrame.from_dict(artist_list)
+    artist_df = artist_df.drop_duplicates( subset=['artist_id'] )
+
+    song_df = pd.DataFrame.from_dict(song_list)
+
+
+    song_df.to_csv("songs.csv", index = False)
 
 
 
